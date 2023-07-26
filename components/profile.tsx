d0a955c9d8715b612617801417e3689ddaf1f239
Ekaji <ekajionah@gmail.com>
2023-07-26 22:11:51 +0100

    update

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,20 +3,23 @@ import { ProfileButton } from './buttons.components'
 import { useSession } from 'next-auth/react';
 
 interface Session {
+  user?: {
     name?: string | null | undefined;
     email?: string | null | undefined;
     image?: string | null | undefined;
     role?: string | null | undefined;
+    id?: string | null | undefined;
+  } | null | undefined;
 } 
 
 export default function Profile() {
-  const { data: session }  = useSession();
+  const { data: session }: any = useSession(); //fix any
   const role = session?.user?.role ?? null;
   const id = session?.user?.id ?? null;
 
   return (
     <ProfileButton role={role} id={id} >
-      <img className="inline-block h-[2.875rem] w-[2.875rem] rounded-full ring-2 ring-white dark:ring-gray-800" src={`https://robohash.org/${1}?set=set2&size=180x180`} alt="Image Description"></img>
+      <img className="inline-block h-[2.875rem] w-[2.875rem] rounded-full ring-2 ring-white dark:ring-gray-800" src={`https://robohash.org/${id}?set=set2&size=180x180`} alt="Image Description"></img>
     </ProfileButton>
   )
 }
